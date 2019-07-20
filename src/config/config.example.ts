07619b0a29a4f1d59586861a3f34af08b23934aa
Varga Zsolt <v.zsozsesz@gmail.com>
2019-07-20 20:17:26 +0200

    hot fix

--- src/config/config.example.ts
+++ src/config/config.example.ts
@@ -1,13 +1,13 @@
 import { TypeOrmModuleOptions } from '@nestjs/typeorm';
 
-export const JWT_SECRET  =  'secret123456789';
+export const JWT_SECRET  =  'secret';
 export const JWT_EXPIRESIN = '12h';
 export const TYPEORM: TypeOrmModuleOptions =  {
     type: 'mysql',
     host: 'localhost',
     port: 3306,
-    username: 'root',
-    password: '',
+    username: 'username',
+    password: 'password',
     database: 'todo',
     entities: ['src/**/*.entity{.ts,.js}'],
     synchronize: true,
--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,13 +1,17 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, UseGuards, SetMetadata } from '@nestjs/common';
 import { UserService } from './user.service';
 import { User } from './entities/user.entity';
+import { AuthGuard } from '@nestjs/passport';
+import { RolesGuard } from 'src/common/guards/roles.guard';
 
+@UseGuards(AuthGuard('jwt'), RolesGuard)
 @Controller('user')
 export class UserController {
 
     constructor(private readonly userService: UserService) {}
 
     @Get()
+    @SetMetadata('roles', ['ADMIN'])
     findAll(): Promise<User[]> {
         return this.userService.findAll();
     }
